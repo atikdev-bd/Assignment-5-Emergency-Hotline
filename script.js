eae393e77b56cc56e8c3b2d5fcbb5631c1f97ca1
atikdev <atikdev.bdd@gamil.com>
2025-08-28 19:08:55 +0600

    when click call button then create a new element and added append child  to parent div done

--- script.js
+++ script.js
@@ -23,7 +23,45 @@ for (let i = 0; i < buttons.length; i++) {
 function setAlert(id1, id2) {
     const serviceTitle = document.getElementById(id1).innerText;
     const serviceNumber = document.getElementById(id2).innerText;
+    const gemPoint = parseInt(document.getElementById('gem-point').innerText);
+    console.log(gemPoint);
+    if (gemPoint < 20) {
+        alert('You have no enough gem to call. Please recharge your gem.');
+        return;
+    }
     alert('Calling' + ' ' + serviceTitle + ' ' + serviceNumber + '...');
+
+    let updateGem = gemPoint - 20;
+    document.getElementById('gem-point').innerText = updateGem;
+
+    let now = new Date();
+
+    let time = now.toLocaleTimeString();
+    console.log(time);
+
+    // 1. Get parent div
+    let parent = document.getElementById("parent-section");
+
+    // 2. Create a new element
+    let newCardHistory = document.createElement("div");
+
+    // 3. Add Tailwind classes
+    newCardHistory.className = "flex justify-between items-center gap-2 mt-4 bg-slate-100 p-4";
+
+    // 4. Set inner HTML with dynamic values
+    newCardHistory.innerHTML = `
+        <div>
+            <h1 class="text-[16px] lg:text-[18px] my-3 font-semibold">${serviceTitle}</h1>
+            <p>${serviceNumber}</p>
+        </div>
+        <div>
+            <p id="current-time">${time}</p>
+        </div>
+  `;
+
+    // 5. Append into parent
+    parent.appendChild(newCardHistory);
+
 
 
 }
@@ -58,7 +96,6 @@ document.getElementById("call-btn-anti-corruption").addEventListener("click", fu
     setAlert('anti-corruption-title', 'anti-corruption-number');
 
 });
-
 
 
 document.getElementById("call-btn-electricity-helpline").addEventListener("click", function () {
